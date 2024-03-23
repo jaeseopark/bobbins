4357778fce5db58a59607d7b55d88bb864f840fd
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2024-03-23 15:54:02 -0600

    fix: some styling issues in the chat comp

--- ui/src/components/chat/Chat.tsx
+++ ui/src/components/chat/Chat.tsx
@@ -10,6 +10,8 @@ import apiclient from "../../apiclient";
 import { signal, useSignal } from "@preact/signals";
 import MessageType from "@minchat/react-chat-ui/dist/types/MessageType";
 
+import "./Chat.scss";
+
 const sigMessages = signal<MessageType[]>([]);
 
 const getUpdatedMessageArray = (messages: MessageType[], user: string, message: string): MessageType[] => {
@@ -32,24 +34,31 @@ const Chat = () => {
     sigShowTypingIndicator.value = true;
     sigMessages.value = getUpdatedMessageArray(sigMessages.value, "HUMAN", question);
 
-    apiclient.ask({ question }).then(({ answer }) => {
-      sigMessages.value = getUpdatedMessageArray(sigMessages.value, "BOT", answer);
-    }).finally(() => {
-      sigShowTypingIndicator.value = false;
-    });
+    apiclient
+      .ask({ question })
+      .then(({ answer }) => {
+        sigMessages.value = getUpdatedMessageArray(sigMessages.value, "BOT", answer);
+      })
+      .finally(() => {
+        sigShowTypingIndicator.value = false;
+      });
   };
 
   return (
     <MinChatUiProvider>
-      <MainContainer>
+      <MainContainer style={{height: "calc(100vh - 5em)"}}>
         <MessageContainer>
           <MessageHeader />
-          <MessageList showTypingIndicator={sigShowTypingIndicator.value} currentUserId="HUMAN" messages={sigMessages.value} />
+          <MessageList
+            showTypingIndicator={sigShowTypingIndicator.value}
+            currentUserId="HUMAN"
+            messages={sigMessages.value}
+          />
           <MessageInput onSendMessage={handleSend} showSendButton placeholder="Type message here" />
         </MessageContainer>
       </MainContainer>
     </MinChatUiProvider>
   );
-}
+};
 
 export default Chat;
